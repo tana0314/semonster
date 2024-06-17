4d873186a3a39e3d25d28d41c2c7e80022f7c51d
tana0314 <e1q21063@oit.ac.jp>
2024-06-17 11:53:02 +0900

    MainとMosterをcommit

--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
 public class Main {
-
+  public static void main(String[] args) {
+    System.out.println("SEMonster!");
+    Monster monster = new Monster();
+    System.out.println(monster.toString());
+  }
 }
